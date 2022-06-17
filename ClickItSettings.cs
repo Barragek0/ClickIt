81b4583ea568d0f53796482f25871b2ff3d7a666
Barragek0 <barragek0lol@gmail.com>
2022-06-17 12:25:40 +0100

    Add linebreaks

--- ClickItSettings.cs
+++ ClickItSettings.cs
@@ -44,7 +44,7 @@ namespace ClickIt
         [Menu("Open Inventory Hotkey", "Hotkey to open your inventory", 8, 3000)]
         public HotkeyNode OpenInventoryKey { get; set; } = new HotkeyNode(Keys.I);
 
-        [Menu("Inventory Open Delay", "Milliseconds to wait after opening the inventory, must be above 200ms as the game takes a short time to open and load the window, disabling clicks during that period. This process may take longer if you're running the game on older hardware or on a HDD, which is the reason for the slider.", 9, 3000)]
+        [Menu("Inventory Open Delay", "Milliseconds to wait after opening the inventory,\nmust be above 200ms as the game takes a short time to open and load the window,\ndisabling clicks during that period.\n\nThis process may take longer if you're running the game on older hardware or on a HDD, which is the reason for the slider.", 9, 3000)]
         public RangeNode<int> InventoryOpenDelayInMs { get; set; } = new RangeNode<int>(200, 200, 800);
         [Menu("Corrupt Essences", "Corrupt essences automatically (misery, envy, dread, scorn)", 10, 3000)]
         public ToggleNode CorruptEssences { get; set; } = new ToggleNode(true);
