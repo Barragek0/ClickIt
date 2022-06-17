163b9ca0de3746e84958ec7ba1ccec7b1866579a
Barragek0 <barragek0lol@gmail.com>
2022-06-17 14:01:06 +0100

    Reorder settings

--- ClickItSettings.cs
+++ ClickItSettings.cs
@@ -25,34 +25,35 @@ namespace ClickIt
         public ButtonNode ReloadPluginButton { get; set; } = new ButtonNode();
         [Menu("Cache Refresh Interval","How often the ground item cache refreshes. Higher value will typically mean less CPU load on your system, but more chance to missclick labels.", 3, 2000)]
         public RangeNode<int> CacheInterval { get; set; } = new RangeNode<int>(30, 0, 200);
+
         [Menu("", 3000)]
         public EmptyNode EmptyClicking { get; set; } = new EmptyNode();
-        [Menu("Click Radius","Ground item label search radius, above 80 is not recommended typically as the plugin may missclick on the interface.", 1, 3000)]
+        [Menu("Search Radius","Radius the plugin will search in for interactable objects. Above 80 is not recommended typically as the plugin may missclick on the interface.", 1, 3000)]
         public RangeNode<int> ClickDistance { get; set; } = new RangeNode<int>(80, 0, 200);
+        [Menu("Items", "Click items", 2, 3000)]
+        public ToggleNode ClickItems { get; set; } = new ToggleNode(true);
+        [Menu("Ignore Unique Items", "Ignore unique items, aside from metamorph organs", 3, 3000)]
+        public ToggleNode IgnoreUniques { get; set; } = new ToggleNode(false);
+        [Menu("Essences", "Click essences", 4, 3000)]
+        public ToggleNode ClickEssences { get; set; } = new ToggleNode(true);
+        [Menu("Corrupt Essences", "Corrupt essences automatically (misery, envy, dread, scorn).\n\nAlso corrupts certain essences that result in a profit when corrupted to deafening.", 5, 3000)]
+        public ToggleNode CorruptEssences { get; set; } = new ToggleNode(true);
         //[Menu("Not yet implemented", 2, 3000)]
         //public ToggleNode MouseProximityMode { get; set; } = new ToggleNode(false);
-        [Menu("Basic Chests", "Click normal (non-league related) chests", 3, 3000)]
+        [Menu("Basic Chests", "Click normal (non-league related) chests", 6, 3000)]
         public ToggleNode ClickBasicChests { get; set; } = new ToggleNode(false);
-        [Menu("League Mechanic 'Chests'", "Click league mechanic related 'chests' (blight pustules, legion war hoards / chests, sentinel caches, etc)", 4, 3000)]
+        [Menu("League Mechanic 'Chests'", "Click league mechanic related 'chests' (blight pustules, legion war hoards / chests, sentinel caches, etc)", 7, 3000)]
         public ToggleNode ClickLeagueChests { get; set; } = new ToggleNode(false);
-        [Menu("Area Transitions", "Click area transitions", 5, 3000)]
-        public ToggleNode ClickAreaTransitions { get; set; } = new ToggleNode(true);
-        [Menu("Shrines", "Click shrines", 6, 3000)]
+        [Menu("Shrines", "Click shrines", 8, 3000)]
         public ToggleNode ClickShrines { get; set; } = new ToggleNode(true);
-        [Menu("Essences", "Click essences", 7, 3000)]
-        public ToggleNode ClickEssences { get; set; } = new ToggleNode(true);
-        [Menu("Open Inventory Hotkey", "Hotkey to open your inventory", 8, 3000)]
+        [Menu("Area Transitions", "Click area transitions", 9, 3000)]
+        public ToggleNode ClickAreaTransitions { get; set; } = new ToggleNode(true);
+        [Menu("Open Inventory Hotkey", "Hotkey to open your inventory", 10, 3000)]
         public HotkeyNode OpenInventoryKey { get; set; } = new HotkeyNode(Keys.I);
 
-        [Menu("Inventory Open Delay", "Milliseconds to wait before searching for remnants, after opening the inventory.\nMust be above 200ms as the game takes a short time to open and load the window, disabling clicks during that period.\n\nThis process may take longer if you're running the game on older hardware or on a HDD, which is the reason for the slider.\n\nIf your inventory is being opened and closed more than once per corruption, this value is too low.", 9, 3000)]
+        [Menu("Inventory Open Delay", "Milliseconds to wait before searching for remnants, after opening the inventory.\nMust be above 200ms as the game takes a short time to open and load the window, disabling clicks during that period.\n\nThis process may take longer if you're running the game on older hardware or on a HDD, which is the reason for the slider.\n\nIf your inventory is being opened and closed more than once per corruption, this value is too low.", 11, 3000)]
         public RangeNode<int> InventoryOpenDelayInMs { get; set; } = new RangeNode<int>(200, 200, 800);
-        [Menu("Corrupt Essences", "Corrupt essences automatically (misery, envy, dread, scorn).\n\nAlso corrupts certain essences that result in a profit when corrupted to deafening.", 10, 3000)]
-        public ToggleNode CorruptEssences { get; set; } = new ToggleNode(true);
-        [Menu("Items", "Click items", 11, 3000)]
-        public ToggleNode ClickItems { get; set; } = new ToggleNode(true);
-        [Menu("Ignore Unique Items", "Ignore unique items, aside from metamorph organs", 12, 3000)]
-        public ToggleNode IgnoreUniques { get; set; } = new ToggleNode(false);
-        [Menu("Block on Open UI", "Disables clicking when certain panels are open to avoid misclicks", 13, 3000)]
+        [Menu("Block on Open UI", "Disables clicking when certain panels are open to avoid misclicks", 12, 3000)]
         public ToggleNode BlockOnOpenLeftRightPanel { get; internal set; } = new ToggleNode(true);
 
     }
