2ee6aabb310b1fd39f0230ea48addab28401af75
Barragek0 <barragek0lol@gmail.com>
2023-12-13 16:51:37 +0000

    Remove unnecessary imports

--- PrimaryAltarComponent.cs
+++ PrimaryAltarComponent.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExileCore.PoEMemory;
-using static ClickIt.ClickIt;
+﻿using static ClickIt.ClickIt;
 
 namespace ClickIt
 {
