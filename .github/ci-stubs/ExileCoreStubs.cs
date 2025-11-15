4015b90fbbadb93555c10ef3765fc2a44b1694a7
Barragek0 <barragek0lol@gmail.com>
2025-11-15 02:47:24 +0000

    Enable integration tests by default; add CI stubs; fix tests and WeightCalculator

---
+++ .github/ci-stubs/ExileCoreStubs.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using SharpDX;
+
+namespace ExileCore
+{
+    public class GameController { }
+    public class Graphics { }
+    public class BaseSettingsPlugin<T> { }
+    public class Coroutine { }
+}
+
+namespace ExileCore.Shared
+{
+    public static class Helpers { }
+    public enum SomeEnum { None }
+}
+
+namespace ExileCore.Shared.Cache
+{
+    public class TimeCache<T>
+    {
+        public TimeCache() { }
+    }
+}
+
+namespace ExileCore.Shared.Enums
+{
+    public enum EntityType { None = 0 }
+}
+
+namespace ExileCore.PoEMemory
+{
+    public class Element
+    {
+        public bool IsValid { get; set; }
+        public string Text { get; set; }
+        public RectangleF GetClientRect() => default(RectangleF);
+    }
+
+    namespace Elements
+    {
+        public class LabelOnGround : Element
+        {
+            public Vector2 GetWorldPos() => default(Vector2);
+        }
+    }
+
+    namespace MemoryObjects
+    {
+        public class Entity { }
+        public class Camera { }
+    }
+
+    namespace Components
+    {
+        public class SomeComponent { }
+    }
+}
+
+// Minimal placeholders for other referenced assemblies (types may be light)
+namespace ExileCore.Shared.Nodes
+{
+    public class ToggleNode<T>
+    {
+        public T Value { get; set; }
+        public ToggleNode(T v) { Value = v; }
+    }
+}
+
+namespace ExileCore.Shared.Attributes
+{
+    public class MenuAttribute : Attribute
+    {
+        public MenuAttribute(string name, int priority = 0) { }
+    }
+}
+
+// Avoid exposing heavy APIs; these stubs exist only for compile-time linking in CI
