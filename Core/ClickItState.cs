66e2c3aa5cf12b4b302090fa8408e18c8a11be6f
Barragek0 <barragek0lol@gmail.com>
2025-11-20 04:31:47 +0000

    refactor: Rename ClickItState to PluginContext for better naming
    - Rename ClickItState.cs to PluginContext.cs (file already added in previous commit)
    - PluginContext better reflects its role as the runtime composition root
    - Add ShrineTimer to PluginContext for shrine performance monitoring

---
+++ Core/ClickItState.cs
@@ -0,0 +1,40 @@
+using ExileCore.PoEMemory.Elements;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared;
+using ExileCore.Shared.Cache;
+using System.Diagnostics;
+
+namespace ClickIt
+{
+    public class ClickItState
+    {
+        public Utils.PerformanceMonitor? PerformanceMonitor { get; set; }
+        public Utils.ErrorHandler? ErrorHandler { get; set; }
+        public Random Random { get; } = new Random();
+        public TimeCache<List<LabelOnGround>>? CachedLabels { get; set; }
+        public Coroutine? AltarCoroutine { get; set; }
+        public Coroutine? ClickLabelCoroutine { get; set; }
+        public Coroutine? DelveFlareCoroutine { get; set; }
+        public Coroutine? ShrineCoroutine { get; set; }
+        public Coroutine? InputSafetyCoroutine { get; set; }
+        public bool IsInputCurrentlyBlocked { get; set; } = false;
+        public Stopwatch LastHotkeyReleaseTimer { get; } = new Stopwatch();
+        public Stopwatch LastRenderTimer { get; } = new Stopwatch();
+        public Stopwatch LastTickTimer { get; } = new Stopwatch();
+        public Stopwatch Timer { get; } = new Stopwatch();
+        public Stopwatch SecondTimer { get; } = new Stopwatch();
+        public const int HOTKEY_RELEASE_FAILSAFE_MS = 5000;
+        public bool LastHotkeyState { get; set; } = false;
+        public Services.AreaService? AreaService { get; set; }
+        public Services.AltarService? AltarService { get; set; }
+        public Services.ShrineService? ShrineService { get; set; }
+        public Utils.InputHandler? InputHandler { get; set; }
+        public Rendering.DebugRenderer? DebugRenderer { get; set; }
+        public Rendering.AltarDisplayRenderer? AltarDisplayRenderer { get; set; }
+        public Utils.DeferredTextQueue? DeferredTextQueue { get; set; }
+        public Utils.InputSafetyManager? InputSafetyManager { get; set; }
+        public Services.ClickService ClickService { get; set; } = null!;
+        public Camera? Camera { get; set; }
+        public bool WorkFinished { get; set; } = false;
+    }
+}
