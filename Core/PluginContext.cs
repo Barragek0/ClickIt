1084e8c516e0e0011b9321bc29694f2dfbceda25
Barragek0 <barragek0lol@gmail.com>
2025-11-21 07:18:28 +0000

    Whitespace change

--- Core/PluginContext.cs
+++ Core/PluginContext.cs
@@ -39,7 +39,7 @@ namespace ClickIt
         public Services.ClickService? ClickService { get; set; }
         public Camera? Camera { get; set; }
         public bool WorkFinished { get; set; } = false;
-        
+
         /// <summary>
         /// Indicates whether we're currently in the Render() method.
         /// Used to prevent logging that could cause recursive rendering issues.
