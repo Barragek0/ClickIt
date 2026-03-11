a0284b7a77a2671207854a4f214bf91d03216ee1
Barragek0 <barragek0lol@gmail.com>
2026-03-11 04:54:54 +0000

    refactor: Replace reflection with direct method call in rectangle overlap tests

--- Tests/Unit/LabelFilterServiceRectOverlapTests.cs
+++ Tests/Unit/LabelFilterServiceRectOverlapTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 using SharpDX;
-using System.Reflection;
+using ClickIt.Utils;
 
 namespace ClickIt.Tests.Unit
 {
@@ -11,21 +11,19 @@ namespace ClickIt.Tests.Unit
         [TestMethod]
         public void DoRectanglesOverlap_ReturnsTrue_ForIntersectingRects()
         {
-            var mi = typeof(Services.LabelFilterService).GetMethod("DoRectanglesOverlap", BindingFlags.NonPublic | BindingFlags.Static)!;
             var a = new RectangleF(0, 0, 10, 10);
             var b = new RectangleF(5, 5, 20, 20);
-            var res = (bool)mi.Invoke(null, new object[] { a, b })!;
+            var res = GeometryHelpers.RectanglesOverlapExclusive(a, b);
             res.Should().BeTrue();
         }
 
         [TestMethod]
         public void DoRectanglesOverlap_ReturnsFalse_ForAdjacentNonOverlappingRects()
         {
-            var mi = typeof(Services.LabelFilterService).GetMethod("DoRectanglesOverlap", BindingFlags.NonPublic | BindingFlags.Static)!;
             var a = new RectangleF(0, 0, 10, 10);
             // Right edge == Left edge should be non-overlapping per implementation
             var b = new RectangleF(10, 0, 20, 10);
-            var res = (bool)mi.Invoke(null, new object[] { a, b })!;
+            var res = GeometryHelpers.RectanglesOverlapExclusive(a, b);
             res.Should().BeFalse();
         }
     }
