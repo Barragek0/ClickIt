4d157f741e09b434df3abe7b11827b83f7a7acaf
Barragek0 <barragek0lol@gmail.com>
2026-04-24 02:49:13 +0100

    fix: CI

--- .github/ci-stubs/ClickItStub.cs
+++ .github/ci-stubs/ClickItStub.cs
@@ -1,4 +1,9 @@
-﻿namespace ClickIt
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClickIt
 {
     namespace Features.Altars
     {
